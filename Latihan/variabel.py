9a722c9c133da2156be5ff7b5208a7a2f45b5903
ayubsukses <ayub.sukses04@gmail.com>
2022-09-14 10:57:50 +0700

    latihan variabel

---
+++ Latihan/variabel.py
@@ -0,0 +1,28 @@
+# inisiasi satu-satu
+a = 1
+b = 2
+namaDepan = 'Ayub'
+_umur = 21
+
+print("---iniasiasi satu satu---")
+print('nilai a: ', a)
+print('nilai b: ', b)
+print('nilai namaDepan: ', namaDepan)
+print('nilai _umur: ', _umur)
+
+# inisiasi banyak
+c, d, e = 3, 4, "inisiasi banyak"
+print("\n---iniasiasi banyak---")
+print('nilai c: ', c)
+print('nilai d: ', d)
+print('nilai e: ', e)
+
+# inisiasi bernilai sama
+
+f = g = h = 8
+print("\n---iniasiasi bernilai sama---")
+print('nilai f: ', f)
+print('nilai g: ', g)
+print('nilai h: ', h)
+
+
